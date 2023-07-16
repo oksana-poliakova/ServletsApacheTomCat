8b5dcddb447064882eb27e1dd8fd6803ea2b7669
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-07-16 13:23:36 +0300

    Created the first servlet

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,40 @@
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Oksana Poliakova on 16.07.2023
+ * @projectName MavenApache
+ */
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config); // Call the init() method of the superclass (HttpServlet).
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");  // Set the content type of the response to "text/html"
+        try (PrintWriter writer = resp.getWriter()) {
+            // Write the HTML content to the response writer.
+            writer.write("<h1>Hello from First Servlet</h1>");
+            writer.write("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // Call the destroy() method of the superclass (HttpServlet).
+        super.destroy();
+    }
+}
